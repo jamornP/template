fcfdcf56bd7b89af14debcef57d3bca14c543276
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-08 08:17:22 +0700

    up

--- function/function.php
+++ function/function.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
- function datethai($date){
+function datethai($date){
     $da=explode("-",$date);
 
     $d=$da[2];
@@ -12,7 +11,34 @@ session_start();
     return  $data;
 
 }
- function month($date){
+function datethaifull($date){
+    $da=explode("-",$date);
+
+    $d=$da[2];
+    $m=$da[1];
+    $y=$da[0];
+    $month=monthfull($date); 
+    $year=year($date);   
+    $data =intval($d)." ".$month." พ.ศ. ".$year;
+    return  $data;
+
+}
+function datethai_time($date){
+    $da_time =explode(" ",$date);
+    $date1 = $da_time[0];
+    $time1 = $da_time[1];
+    $da=explode("-",$date1);
+
+    $d=$da[2];
+    $m=$da[1];
+    $y=$da[0];
+    $month=month($date); 
+    $year=year($date);   
+    $data =intval($d)." ".$month." ".$year." ".$time1;
+    return  $data;
+
+}
+function month($date){
     $da=explode("-",$date);
     $d=$da[2];
     $m=$da[1];
@@ -58,6 +84,52 @@ session_start();
     }
     return $month;
 }
+function monthfull($date){
+    $da=explode("-",$date);
+    $d=$da[2];
+    $m=$da[1];
+    $y=$da[0];
+    switch ($m){
+        case "01":
+            $month="มกราคม";
+            break;
+        case "02":
+            $month="กุมภาพันธ์";
+            break;
+        case "03":
+            $month="มีนาคม";
+            break;
+        case "04":
+            $month="เมษายน";
+            break;
+        case "05":
+            $month="พฤษภาคม";
+            break;
+        case "06":
+            $month="มิถุนายน";
+            break;
+        case "07":
+            $month="กรกฎาคม";
+            break;
+        case "08":
+            $month="สิงหาคม";
+            break;
+        case "09":
+            $month="กันยายน";
+            break;
+        case "10":
+            $month="ตุลาคม";
+            break;
+        case "11":
+            $month="พฤศจิกายน";
+            break;
+        case "12":
+            $month="ธันวาคม";
+            break;
+        
+    }
+    return $month;
+}
  function day($date){
     $da=explode("-",$date);
     $d=$da[2];
@@ -97,7 +169,7 @@ function yearterm($date){
      return  $y+543;
 
 }
-function time_dif($begin,$end){
+function time_dif_EN($begin,$end){
 	$remain=intval(strtotime($end)-strtotime($begin));
 	$wan=floor($remain/86400);
 	$l_wan=$remain%86400;
@@ -105,77 +177,198 @@ function time_dif($begin,$end){
 	$l_hour=$l_wan%3600;
 	$minute=floor($l_hour/60);
 	$second=$l_hour%60;
-	return "ผ่านมาแล้ว ".$wan." วัน ".$hour." ชั่วโมง ".$minute." นาที ".$second." วินาที";
+    if($wan>0){
+        $data = $wan." d ".$hour." h ".$minute." m ".$second." seconds ago";
+    }elseif($hour >0){
+        $data = $hour." h ".$minute." m ".$second." seconds ago";
+    }elseif($minute >0){
+        $data = $minute." m ".$second." seconds ago";
+    }elseif($second >0){
+        $data = $second." seconds ago";
+    }else{
+        $data= "Now";
+    }
+	return $data;
+}
+function time_dif_TH($begin,$end){
+	$remain=intval(strtotime($end)-strtotime($begin));
+	$wan=floor($remain/86400);
+	$l_wan=$remain%86400;
+	$hour=floor($l_wan/3600);
+	$l_hour=$l_wan%3600;
+	$minute=floor($l_hour/60);
+	$second=$l_hour%60;
+    if($wan>0){
+        $data = $wan." วัน ".$hour." ชั่วโมง ".$minute." นาที ".$second." วินาที";
+    }elseif($hour >0){
+        $data = $hour." ชั่วโมง ".$minute." นาที ".$second." วินาที";
+    }elseif($minute >0){
+        $data = $minute." นาที ".$second." วินาที";
+    }elseif($second >0){
+        $data = $second." วินาที";
+    }else{
+        $data= "Now";
+    }
+	return $data;
 }
 function statusRepair($s){
-    switch ($s) {
+    
+    switch ($s['s_id']) {
         
     case "1":
-        return 260;
+        // แจ้ง
+        $da['color']="bg-orange";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>call</i></button>";
+        return $da;
         break;
     case "2":
-        return 30;
+        // รับเรื่อง
+        $da['color']="bg-cyan";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>save</i></button>";
+        return $da;
         break;
     case "3":
-        return 270;
+        // จ่ายงาน
+        $da['color']="bg-light-blue";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>person</i></button>";
+        return $da;
         break;
     case "4":
-        return 130;
+        // สำรวจหน้างาน
+        $da['color']="bg-blue";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>directions_run</i></button>";
+        return $da;
         break;
     case "5":
-        return 164;
+        // ดำเนินการซ่อม
+        $da['color']="bg-indigo";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>build</i></button>";
+        return $da;
         break;
     case "6":
-        return 107;
+        // ซ่อมไม่ได้
+        $da['color']="bg-deep-orange";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>warning</i></button>";
+        return $da;
         break;
     case "7":
-        return 205;
+        // ซ่อมเสร็จ
+        $da['color']="bg-light-green";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12 ' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>camera_alt</i></button>";
+        return $da;
         break;
     case "8":
-        return 210;
+        // เรียบร้อย
+        $da['color']="bg-teal";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>done</i></button>";
+        return $da;
         break;
     case "9":
-        return 138;
+        // รออะไหล่
+        $da['color']="bg-grey";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>schedule</i></button>";
+        return $da;
         break;
     case "10":
-        return 151;
+        // รอจ้างเหมา
+        $da['color']="bg-blue-grey";
+        $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>monetization_on</i></button>";
+        return $da;
         break;
     default:
-        return 1;
+        
     }
 }
-function navMenu($page){
-    switch ($page){
-        case "home":
-            $_SESSION['m_home']="active";
-            $_SESSION['m_activity']="";
-            $_SESSION['m_download']="";
-            $_SESSION['m_manual']="";
+function statusIT($s){
+    
+    switch ($s['s_id']) {
+        
+        case "1":
+            // แจ้ง
+            $da['color']="bg-orange";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>call</i></button>";
+            return $da;
             break;
-        case "activity":
-            $_SESSION['m_home']="";
-            $_SESSION['m_activity']="active";
-            $_SESSION['m_download']="";
-            $_SESSION['m_manual']="";
+        case "2":
+            // รับเรื่อง
+            $da['color']="bg-cyan";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>save</i></button>";
+            return $da;
             break;
-        case "download":
-            $_SESSION['m_home']="";
-            $_SESSION['m_activity']="";
-            $_SESSION['m_download']="active";
-            $_SESSION['m_manual']="";
+        case "3":
+            // ดำเนินการซ่อม
+            $da['color']="bg-indigo";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>build</i></button>";
+            return $da;
             break;
-        case "manual":
-            $_SESSION['m_home']="";
-            $_SESSION['m_activity']="";
-            $_SESSION['m_download']="";
-            $_SESSION['m_manual']="active";
+        case "4":
+            // ซ่อมไม่ได้
+            $da['color']="bg-deep-orange";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>warning</i></button>";
+            return $da;
             break;
-        default :
-            $_SESSION['m_home']="active";
-            $_SESSION['m_activity']="";
-            $_SESSION['m_download']="";
-            $_SESSION['m_manual']="";
+        case "5":
+            // รอดำเนินการครั้งถัดไป
+            $da['color']="bg-light-green";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12 ' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>camera_alt</i></button>";
+            return $da;
             break;
+        case "8":
+            // เรียบร้อย
+            $da['color']="bg-teal";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>done</i></button>";
+            return $da;
+            break;
+        case "7":
+            // รออะไหล่
+            $da['color']="bg-grey";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>schedule</i></button>";
+            return $da;
+            break;
+        case "6":
+            // ส่งบริษัท
+            $da['color']="bg-blue-grey";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>monetization_on</i></button>";
+            return $da;
+            break;
+        case "9":
+            // ไม่ซ่อม
+            $da['color']="bg-deep-orange";
+            $da['bt']="<button type='button' class='btn btn-xs {$da['color']} waves-effect mb-2'><i class='material-icons fs-12' data-toggle='tooltip' data-placement='top' title='{$s['s_name']}'>highlight_off</i></button>";
+            return $da;
+            break;
+        default:
+            
+        }
+}
+function go($data){
+    $code = base64_encode($data);
+    return $code;
+}
+function to($data){
+    $code = base64_decode($data);
+    return $code;
+}
+function btStatus($data){
+    $s="";
+    foreach($data as $c){
+        $ds['s_id'] = $c['s_id'];
+        $ds['s_name'] = $c['s_name'];
+        $das = statusRepair($ds);
+        $s = $s."".$das['bt'];
+    
     }
+    return $s;
+}
+function btStatusIT($data){
+    $s="";
+    foreach($data as $c){
+        $ds['s_id'] = $c['s_id'];
+        $ds['s_name'] = $c['s_name'];
+        $das = statusIT($ds);
+        $s = $s."".$das['bt'];
+    
+    }
+    return $s;
 }
 ?>
