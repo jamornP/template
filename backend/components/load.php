301c4e136bc755aa5055c5b6aa4979a271ba8db7
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-08 10:29:59 +0700

    up

--- backend/components/load.php
+++ backend/components/load.php
@@ -1,3 +1,3 @@
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="/template/images/logo.png" alt="Science" height="60" width="60">
+    <img class="animation__shake" src="/template/imges/logo.png" alt="Science" height="70" width="360">
 </div>
